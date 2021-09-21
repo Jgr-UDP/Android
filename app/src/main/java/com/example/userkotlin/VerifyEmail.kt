7d3394d61ab0b2d791686076356d46a329245c97
Jgr-UDP <julio.garate@mail.udp.cl>
2021-09-21 14:49:43 -0300

    Se sube Archivo

---
+++ app/src/main/java/com/example/userkotlin/VerifyEmail.kt
@@ -0,0 +1,19 @@
+package com.example.userkotlin
+
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class VerifyEmail : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.verify_email)
+        val window = this.window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        window.statusBarColor = this.resources.getColor(R.color.colorBlue)
+
+
+    }
+}
